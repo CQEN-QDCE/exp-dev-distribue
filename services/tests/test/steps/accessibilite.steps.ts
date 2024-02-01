b4c4b10db1329d6d3e56956a0eae47bfd5ff10a4
Achille Kamdem <achillealain.kamdemdjoko@mcn.gouv.qc.ca>
2024-01-31 19:28:39 -0500

    Ajout du descriptif des violations des règles a11y

--- services/tests/test/steps/accessibilite.steps.ts
+++ services/tests/test/steps/accessibilite.steps.ts
@@ -3,5 +3,8 @@ import { expect } from '@wdio/globals'
 import { AxeBuilder } from '@axe-core/webdriverio';
 
 Then(/^La page est complement accessible$/, async () => {
-	expect((await new AxeBuilder({ client: browser }).analyze()).violations).toEqual("")
+	const a11yViolations = JSON.stringify((await new AxeBuilder({ client: browser }).analyze()).violations);
+
+	console.log('Violations des règles de l\'accessibilité:' + a11yViolations.toString())
+	expect(a11yViolations).toEqual("")
 });
